667ce8fbce5483670bbbd0879dc1549f4756ef97
bryant <zdu@usc.edu>
2013-03-21 11:05:09 -0800

    Site setup with Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'f0c06fed9690f694e6eb0410fc80adf452cd8787f24d8ab919560b14f60b6a5e43fd4a924464b9c460ed98399bef7bbf0e58115b9334532f9c3256251585739c'
