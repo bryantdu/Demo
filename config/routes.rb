3acf07c96af505547010d8804853362e3ec0f3c6
bryant <zdu@usc.edu>
2013-03-21 18:42:37 -0800

    finish signin

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Demo::Application.routes.draw do
   
   resources :users
   
-  match '/signup', to: 'user#new'
+  resources :sessions, only: [:new, :create, :destroy ]
+  
+  match '/signup', to: 'users#new'
+  match '/signin', to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
   
   root :to => "home#index"
 end
